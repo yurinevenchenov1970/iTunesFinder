edc187f824e988cb229675797614d0a4a70c1da5
Yuri Nevenchenov <yurinevenchenov1970@gmail.com>
2017-08-20 19:56:01 +0300

    added search view to activity

--- app/src/main/java/yurinevenchenov1970/github/com/itunesfinder/MainActivity.java
+++ app/src/main/java/yurinevenchenov1970/github/com/itunesfinder/MainActivity.java
@@ -1,13 +1,45 @@
 package yurinevenchenov1970.github.com.itunesfinder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+        createSearchView();
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    private void createSearchView() {
+        SearchView searchView = (SearchView) findViewById(R.id.search);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // TODO: 8/20/2017 use query for api
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
     }
 }
