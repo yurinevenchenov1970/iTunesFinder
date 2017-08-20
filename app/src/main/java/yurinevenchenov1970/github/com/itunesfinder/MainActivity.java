547b9750ff41700e8b1635f770f1893182831f33
Yuri Nevenchenov <yurinevenchenov1970@gmail.com>
2017-08-20 17:21:10 +0300

    initial commit

---
+++ app/src/main/java/yurinevenchenov1970/github/com/itunesfinder/MainActivity.java
@@ -0,0 +1,13 @@
+package yurinevenchenov1970.github.com.itunesfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
